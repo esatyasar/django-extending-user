b81a101288647a5e5c366b5318723bfd0e771e5f
esatyasar <esatyasar91@gmail.com>
2021-10-23 16:13:10 +0300

    session 1

--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, redirect, HttpResponse
+from .forms import UserForm
 
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def home(request):
     return render(request, 'users/home.html')
+
+def register(request):
+    form  = UserForm()
+    if request.method =='POST':
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password = password)
+            login(request,user)
+            return redirect('home')
+        
+    context = {
+        'form_user' : form
+    }
+    
+    return render(request, "users/register.html")
